45d1ccc2fdd939f9979466ce8f971a7c00bc8cc6
Davidcasstella <brayan.lopez07@uptc.edu.co>
2026-03-10 10:58:12 -0500

    Se hacer los ultimos ajustes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static('public'));
 
-// ==================== CONFIGURACIÓN DE MULTER ====================
+// ==================== CONFIGURACIÓN DE MULTERR ====================
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
